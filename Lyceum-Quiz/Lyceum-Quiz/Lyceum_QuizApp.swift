0ee16b2780efee8d8e018047ca878828eb696019
Ambermate <ambermate.tech@gmail.com>
2023-05-16 12:49:28 -0700

    init commit

---
+++ Lyceum-Quiz/Lyceum-Quiz/Lyceum_QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lyceum_QuizApp.swift
+//  Lyceum-Quiz
+//
+//  Created by Long Hai on 5/16/23.
+//
+
+import SwiftUI
+
+@main
+struct Lyceum_QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
